91fc79630d791495f78fa44c212bae67bba3c70d
Faranez <zz.faranez@gmail.com>
2022-01-29 00:00:24 +0300

    Object_Calc

---
+++ main3.js
@@ -0,0 +1,20 @@
+function calc (operator,a,b) {
+    let conteiner = {
+        sum: a + b,
+        sub: a - b,
+        mult: a * b,
+        div: a / b,
+};
+        if (typeof a == 'number' && typeof b == 'number' && conteiner[operator] !== undefined) {
+            
+    console.log(conteiner[operator]);
+ }
+ else {
+     console.log('Error');
+  };
+};
+
+calc('sum',2,2);
+calc('sub',2,1);
+calc('div',1,1);
+calc('div',2,1);
